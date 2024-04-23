fda2e9a8cd7c8c4592dfc00da2060293a6f2e1fe
Apricityx <Apricityx@qq.com>
2024-04-23 09:32:51 +0800

    Feat. 重构服务端，将每一个连接都实例化为一个对象，方便管理

--- ws_server.py
+++ ws_server.py
@@ -1,11 +1,13 @@
 import asyncio
+import time
+
 import websockets
 import pymysql as sql
 import json
 import ssl
 
 database_url = 'localhost'
-clients = []
+clients = {}
 
 
 def debug(content):
@@ -52,7 +54,53 @@ class DB:
 database = DB()
 
 
+def client_del(client):
+    global clients
+    clients.pop(client.self_id)
+    del client
+
+
 # database.post_message("zhangsan", "HELLO")
+class WebSocketClient:
+    ws_core = None  # ws_core是一个可遍历的对象，可以用来接收消息
+    self_id = None
+
+    # self_clock = 10  # 10秒没收到ping包就认为掉线
+
+    async def client_start(self):
+        debug("WebSocket client start")
+
+    def __init__(self, websocket, self_id):
+        self.ws_core = websocket
+        self.self_id = self_id
+        asyncio.ensure_future(self.client_start())
+
+    async def send(self, raw_message):  # 此处raw_message是字典
+        await self.ws_core.send(json.dumps(raw_message))
+        debug(f"Sent client ws message: {raw_message}")
+
+    async def on_message(self, raw_message):  # 此处raw_message是字典
+        message = raw_message
+        debug(f"Received client ws message: {message}")
+        if message["method"] == "ping":
+            print("新连接建立")
+        # on_websocket_send 定义有以下方法pull push update
+        elif message["method"] == "pull":
+            count = message["pullNum"]
+            result = {"method": "pull", "data": database.pull_message(count)}
+            debug(f"pull_message: {result}")
+            await self.send(result)  # 这里数据发不出去
+            return result
+        elif message["method"] == "push":
+            debug("pushing")
+            data = message["data"]
+            nickname = data["nickname"]
+            message = data["message"]
+            database.post_message(nickname, message)
+            await send_update_to_all()
+        elif message["method"] == "ping":
+            debug("ping")
+            await self.send(json.dumps({"method": "ping", "data": "pong!!"}))  # 未实现
 
 
 async def check_online():
@@ -76,59 +124,66 @@ async def check_online():
 
 async def send_update_to_all():
     global clients
-    time = len(clients)
-    del_clients_num = []
-    for i in range(0, time):
+    del_id_list = []
+    for key in clients:
+        client = clients[key]
         debug(f"尝试广播")
-        client = clients[i]
-
         try:
-            await client.send(json.dumps({
+            await client.send({
                 "method": "update",
                 "data": database.pull_message(1)
-            }))
+            })
         except Exception as ConnectionClosedOK:
             debug(f"客户端已断开连接: {ConnectionClosedOK}")
-            del_clients_num.append(i)
+            del_id_list.append(key)
             continue
         except Exception as e:
             debug(f"未知错误: {e}")
+            del_id_list.append(key)
             continue
-    i = 0
-    for client in del_clients_num:
-        clients.pop(client - i)
-        i += 1
-    debug(f"已删除{len(del_clients_num)}个离线客户端")
+    for del_id in del_id_list:
+        clients.pop(del_id)
+    debug(f"已删除：{len(del_id_list)}个离线客户端")
 
 
-async def on_message(message, client):
-    message = json.loads(message)  # 将收到的JSON数据转换为字典
-    debug(f"Received client ws message: {message}")
-    if message["method"] == "ping":
-        print("新连接建立")
-    # on_websocket_send 定义有以下方法pull push update
-    elif message["method"] == "pull":
-        count = message["pullNum"]
-        result = {"method": "pull", "data": database.pull_message(count)}
-        debug(f"pull_message: {result}")
-        await client.send(json.dumps(result))  # 这里数据发不出去
-        return result
-    elif message["method"] == "push":
-        debug("pushing")
-        data = message["data"]
-        nickname = data["nickname"]
-        message = data["message"]
-        database.post_message(nickname, message)
-        await send_update_to_all()
+# async def on_message(message, client):
+#     message = json.loads(message)  # 将收到的JSON数据转换为字典
+#     debug(f"Received client ws message: {message}")
+#     if message["method"] == "ping":
+#         print("新连接建立")
+#     # on_websocket_send 定义有以下方法pull push update
+#     elif message["method"] == "pull":
+#         count = message["pullNum"]
+#         result = {"method": "pull", "data": database.pull_message(count)}
+#         debug(f"pull_message: {result}")
+#         await client.send(json.dumps(result))  # 这里数据发不出去
+#         return result
+#     elif message["method"] == "push":
+#         debug("pushing")
+#         data = message["data"]
+#         nickname = data["nickname"]
+#         message = data["message"]
+#         database.post_message(nickname, message)
+#         await send_update_to_all()
+#     elif message["method"] == "ping":
+#         debug("ping")
+#         await client.send(json.dumps({}))  # 未实现
 
 
 async def server_start(websocket, path):
+    # 传进来一个websocket对象，可以用来发送消息
+    # websocket对象拥有send方法，可以用来发送消息
+    client_id = int(time.time())
+    client = WebSocketClient(websocket, client_id)
     global clients
     # 每一个客户端建立连接都会执行此处的指令
-    clients.append(websocket)
+    clients[client_id] = client
     debug(f"当前在线：{len(clients)}")
-    async for message in websocket:
-        await on_message(message, websocket)
+    await client.send("Hello, world!")
+    async for message in client.ws_core:
+        debug(f"Received client ws message: {message}")
+        message = json.loads(message)  # 将收到的JSON数据转换为字典
+        await client.on_message(message)  # 处理客户端发来的消息
 
 
 # pull_message(1)
@@ -142,6 +197,5 @@ async def server_start(websocket, path):
 #     ssl=ssl_context
 # )
 start_server = websockets.serve(server_start, "localhost", 8765)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
